007c35b83657bb8484aada517322972aaf3e57db
varshil241296 <varshilshah241296@gmail.com>
2021-03-21 17:00:21 +0530

    1 file modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from "@angular/core";
-
+import { HttpClient } from "@angular/common/http";
 @Injectable()
 export class CartService {
   item = [];
-  constructor() {}
+  constructor(private _http: HttpClient) {}
   addToCart(product) {
     this.item.push(product);
     localStorage.setItem("clickCounter", JSON.stringify(this.item));
@@ -11,5 +11,10 @@ export class CartService {
   getItems() {
     this.item = JSON.parse(localStorage.getItem("clickCounter"));
     return this.item;
+  }
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      "/assets/shipping.json"
+    );
   }
 }
